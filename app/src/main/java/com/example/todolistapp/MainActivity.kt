a6de1115d70124e631f94ecb5259c8d555062029
Kimberly Ramos <kimberlyramos45980@yahoo.com>
2022-11-17 14:46:02 -0800

    Button click pops up AlertView/ListView filled with placeholder text

--- app/src/main/java/com/example/todolistapp/MainActivity.kt
+++ app/src/main/java/com/example/todolistapp/MainActivity.kt
@@ -1,11 +1,79 @@
 package com.example.todolistapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.folder_name.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Button click
+        val newFolder = button.setOnClickListener {
+            // Inflate dialog with view
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.folder_name, null)
+            // Alert Dialog Builder
+            val mBuilder = AlertDialog.Builder(this)
+                    .setView(mDialogView)
+                    .setTitle("New Folder Name")
+            // Show dialog
+            val mAlertDialog = mBuilder.show()
+            // Save button click on custom layout
+            mDialogView.save.setOnClickListener {
+                // dismiss alert
+                mAlertDialog.dismiss()
+                // get values from alert dialog
+                val folderName = mDialogView.dialogFolderName.text.toString()
+                // Set inputed text in listView
+            }
+            // Cancel button click on custom layout
+            mDialogView.cancel.setOnClickListener {
+                // dismiss alert
+                mAlertDialog.dismiss()
+            }
+        }
+
+        // Custom adapter for ListView
+        listView.adapter = MyCustomAdapter(this)
+    }
+
+    private class MyCustomAdapter(context: Context): BaseAdapter() {
+
+        private val mContext: Context
+
+        init {
+            mContext = context
+        }
+
+        // Responsible for how many rows in ListView
+        override fun getCount(): Int {
+            return 5
+        }
+
+        // Ignore for now
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        // Ignore for now
+        override fun getItem(position: Int): Any {
+            return "TEST STRING"
+        }
+
+        // responsible for rendering each row in ListView
+        override fun getView(position: Int, convertView: View?, viewGroup: ViewGroup?): View {
+            val textView = TextView(mContext)
+            textView.text = "Test text for list view"
+            return textView
+        }
     }
 }
