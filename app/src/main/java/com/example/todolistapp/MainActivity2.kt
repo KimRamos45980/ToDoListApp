1973c5e3d95229492c7c43c1f919d168c48cf653
Kimberly Ramos <kimberlyramos45980@yahoo.com>
2022-12-08 17:52:00 -0800

    Note activity opens with folder name in the actionBar

--- app/src/main/java/com/example/todolistapp/MainActivity2.kt
+++ app/src/main/java/com/example/todolistapp/MainActivity2.kt
@@ -7,5 +7,15 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val folder = intent.getStringExtra("folderName")
+
+        var actionBar = supportActionBar
+
+        if (actionBar != null) {
+            title = folder
+            actionBar?.setDisplayShowHomeEnabled(true)
+            actionBar?.setDisplayHomeAsUpEnabled(true)
+        }
     }
 }
